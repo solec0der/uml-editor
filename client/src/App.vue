3717672983f5232281a1650b48e274c2b4f954f5
Yannick Huggler <yannick@huggler.io>
2020-12-26 23:44:50 +0100

    #6 - feature(theming): moved header from app to separate header component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,62 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-      <ThemeChangerMenu />
-    </v-app-bar>
-
+    <Header />
     <v-main>
-      <HelloWorld />
-
-      <v-btn color="accent">jfiowejfiwe</v-btn>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-import ThemeChangerMenu from '@/components/ThemeChangerMenu.vue'
+import { Component, Vue } from 'vue-property-decorator'
+import Header from '@/components/Header.vue'
 
-export default Vue.extend({
-  name: 'App',
-
+@Component({
   components: {
-    HelloWorld,
-    ThemeChangerMenu,
+    Header,
   },
-
-  data: () => ({
-    //
-  }),
 })
+export default class App extends Vue {}
 </script>

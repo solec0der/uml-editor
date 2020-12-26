f857a1e5a30e10ab7b2fa27ab4063f4b9e3f18d1
Yannick Huggler <yannick.huggler@polypoint.ch>
2020-12-26 22:01:32 +0100

    #6 - feature(theming): added light and dark theme color palette and a theme switch

---
+++ client/src/components/ThemeChangerMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-menu :close-on-content-click="false" :nudge-width="200" offset-y>
+    <template v-slot:activator="{ on }">
+      <v-btn large icon v-on="on">
+        <v-icon size="30" color="white">mdi-palette</v-icon>
+      </v-btn>
+    </template>
+    <v-card>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="font-weight-bold">
+            Dark Mode
+          </v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-switch v-model="$vuetify.theme.dark" />
+        </v-list-item-action>
+      </v-list-item>
+      <v-divider />
+    </v-card>
+  </v-menu>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class ThemeChangerMenu extends Vue {}
+</script>
--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -3,4 +3,22 @@ import Vuetify from 'vuetify/lib'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({})
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#0097a7',
+        secondary: '#FF3366',
+        accent: '#FF3366',
+        error: '#b71c1c',
+      },
+      dark: {
+        primary: '#0097a7',
+        secondary: '#FF3366',
+        accent: '#FF3366',
+        error: '#b71c1c',
+      },
+    },
+    dark: true
+  }
+})
